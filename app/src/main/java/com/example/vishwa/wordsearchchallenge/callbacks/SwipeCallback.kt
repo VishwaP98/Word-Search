b7b53c16b21e3d37a9c8948b9edd4e67453cf1f0
Vishwa Patel <vishwa.patel@mail.utoronto.ca>
2019-05-14 21:51:44 -0400

    Summary: Designed callback to handle word selection from the gridView. Author: Vishwa Patel

---
+++ app/src/main/java/com/example/vishwa/wordsearchchallenge/callbacks/SwipeCallback.kt
@@ -0,0 +1,13 @@
+package com.example.vishwa.wordsearchchallenge.callbacks
+
+/**
+ * Created by Vishwa Patel
+ */
+
+interface SwipeCallback {
+
+    fun onWordSelected(word: String)
+
+    fun onInvalidWordSelected()
+
+}
